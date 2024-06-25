d13ac8ac6b752374632e5fc74bed05c96ace6555
Якименко Дмитро <108049675+TinyLine@users.noreply.github.com>
2024-06-25 18:51:22 +0300

    ms

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -583,7 +583,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55247" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50937" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
