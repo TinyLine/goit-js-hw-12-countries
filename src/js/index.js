5328b33e943885b476a2c6cdd92ff529f0aa6d50
Якименко Дмитро <108049675+TinyLine@users.noreply.github.com>
2024-06-26 14:34:21 +0300

    ms

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import fetchCountries from './fetchCountries';
-import debounce from './lodash.debounce';
+import debounce from 'lodash.debounce';
 
 const searchInput = document.querySelector('#search');
 const countryList = document.querySelector('#country-list');
@@ -42,6 +42,8 @@ function renderCountries(countries) {
     `)
     .join('');
 }
+
+
 
 
 
